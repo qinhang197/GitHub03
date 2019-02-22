6ff9aee9fb3801e1504d0abd969e24f1548fd0d8
zhansan <123@qq.com>
2019-02-22 20:05:26 +0800

    第二次

--- src/cn/itcast/git01.java
+++ src/cn/itcast/git01.java
@@ -2,5 +2,10 @@ package cn.itcast;
 
 public class git01 {
 
-
+    public static void main(String[] args) {
+        System.out.println("上传了");
+        System.out.println("上传了");
+        System.out.println("上传了");
+        System.out.println("上传了");
+    }
 }
